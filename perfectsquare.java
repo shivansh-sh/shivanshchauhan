a07cda9173f4bafed090082816dba5b98fcbaf15
shivansh-sh <96882189+shivansh-sh@users.noreply.github.com>
2022-01-07 13:36:53 +0530

    Add files via upload

---
+++ perfectsquare.java
@@ -0,0 +1,18 @@
+package perfectsquare;
+import java.util.Scanner;
+public class perfectsquare{
+    public static void main(string[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = scnextInt();
+        int count = 0;
+        if (int f = 1; f <= a; f++){
+            if(a % f ==0){
+                count = count + 1;
+            }
+            if (!(count % 2 ==0));
+            System.out.println("perfect square");
+        }
+
+    }
+
+
